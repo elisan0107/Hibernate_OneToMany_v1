2409601675c889ab483890b68996f0740513fb12
elisan0107 <elisan0107@skola.goteborg.se>
2024-08-14 13:57:09 +0200

    adding PRIMARY KEY to Tutor class

--- src/main/java/se/yrgo/Tutor.java
+++ src/main/java/se/yrgo/Tutor.java
@@ -1,4 +1,11 @@
 package se.yrgo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 public class Tutor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
