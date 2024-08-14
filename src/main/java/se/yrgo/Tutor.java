64b491e37b73c79cd85f746db296ea4b5c571b18
elisan0107 <elisan0107@skola.goteborg.se>
2024-08-14 14:06:27 +0200

    creating a OneToMany relationship between Tutor and Student

--- src/main/java/se/yrgo/Tutor.java
+++ src/main/java/se/yrgo/Tutor.java
@@ -1,11 +1,17 @@
 package se.yrgo;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Tutor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Student> students = new ArrayList<>();
 }
